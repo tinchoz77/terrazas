0b418a090f460093a573a5522bb2634a3c5d112e
TinchoZ <tinchoz@gmail.com>
2021-12-30 19:45:48 -0300

    Corrección de referencia a contratos en migrations

--- migrations/2_edificio_migration.js
+++ migrations/2_edificio_migration.js
@@ -1,5 +1,5 @@
-const Trust  = artifacts.require("Edificio");
+const Edificio  = artifacts.require("Edificio");
 
 module.exports = function (deployer) {
-  deployer.deploy(Trust, '0x75993aBEB66e50bfA20525064237A9Eab0dd4aAb', 2000);
+  deployer.deploy(Edificio);
 };
--- migrations/3_constructora_migration.js
+++ migrations/3_constructora_migration.js
@@ -1,5 +1,5 @@
-const Trust  = artifacts.require("Constructora");
+const Constructora  = artifacts.require("Constructora");
 
 module.exports = function (deployer) {
-  deployer.deploy(Trust, '0x75993aBEB66e50bfA20525064237A9Eab0dd4aAb', 2000);
+  deployer.deploy(Constructora, '0x75993aBEB66e50bfA20525064237A9Eab0dd4aAb', 2000);
 };
